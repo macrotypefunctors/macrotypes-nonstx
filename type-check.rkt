6777f548a38c7a58d5561fa9c2319fd84f9d0416
AlexKnauth <alexander@knauth.org>
2018-02-25 18:36:41 -0500

    add comments on expand/#%var

--- type-check.rkt
+++ type-check.rkt
@@ -6,11 +6,15 @@
          "expand-stop.rkt"
          "type-prop.rkt")
 
+;; expand/#%var : Stx Symbol [Listof Id] -> Stx
+;; Expand stx, but if it comes to one of the xs, put #%var around it
 (define (expand/#%var stx ctx xs)
   (define stx* (expand/stop stx ctx xs))
   (cond
+    ;; x where x ∈ xs
     [(and (identifier? stx*) (member stx* xs free-identifier=?))
      (expand/#%var (datum->syntax stx `(#%var ,stx*) stx stx) ctx xs)]
+    ;; (x . rst) where x ∈ xs
     [(and (stx-pair? stx*) (identifier? (stx-car stx*))
           (member (stx-car stx*) xs free-identifier=?))
      (expand/#%var
