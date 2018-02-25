f4fdab789b81c1c9e7eac5dae73bb986742c9a52
AlexKnauth <alexander@knauth.org>
2018-02-25 18:13:54 -0500

    add stx:also-is

--- prop.rkt
+++ prop.rkt
@@ -3,9 +3,11 @@
 (provide
  ;; X -> [StxIs X]
  stx:is
+ ;; Stx X -> [StxIs X]
+ stx:also-is
  ;; Stx X -> [StxHas X]
  stx:has
- ;; both of these are also match expanders
+ ;; all of these are also match expanders
  )
 
 (require racket/match
@@ -23,29 +25,40 @@
   (cond [(pair? x) (ca*r (car x))]
         [else      x]))
 
+(struct it [value] #:prefab)
+
 ;; --------------------------------------------------------------
 
 ;; A Stx is a syntax object
 
 ;; A [StxIs X] is a syntax object which has a property:
-;;   prop:is : X
+;;   prop:is : (it X)
 (define (stx:is? v)
-  (and (syntax? v) (syntax-property v prop:is) #t))
+  (and (syntax? v) (it? (ca*r (syntax-property v prop:is))) #t))
 
 ;; A [StxHas X] is a syntax object which has a property:
-;;   prop:has-type : X
+;;   prop:has-type : (it X)
 (define (stx:has? v)
-  (and (syntax? v) (syntax-property v prop:has) #t))
+  (and (syntax? v) (it? (ca*r (syntax-property v prop:has))) #t))
 
 ;; --------------------------------------------------------------
 
 ;; X -> [Stx:Is X]
-(define (x->stx:is-x type)
-  (syntax-property #'(stop/error) prop:is type))
+(define (x->stx:is-x x)
+  (stx/x->stx:also-is-x #'(stop/error) x))
 
 ;; [Stx:Is X] -> X
 (define (stx:is-x->x stx)
-  (ca*r (syntax-property stx prop:is)))
+  (it-value (ca*r (syntax-property stx prop:is))))
+
+;; Stx X -> [Stx:Is X]
+(define (stx/x->stx:also-is-x stx x)
+  (syntax-property stx prop:is (it x)))
+
+;; [Stx:Is X] -> (values Stx X)
+(define (stx:also-is-x->stx/x stx)
+  (values (syntax-property stx prop:is #false)
+          (stx:is-x->x stx)))
 
 ;; X -> [Stx:Is X]
 ;; but also a match expander
@@ -54,14 +67,22 @@
     [(_ pat:expr) #'(? stx:is? (app stx:is-x->x pat))])
   (id-transformer (λ (stx) #'x->stx:is-x)))
 
+;; Stx X -> [Stx:Is X]
+;; but also a match expander
+(define-match-expander stx:also-is
+  (syntax-parser
+    [(_ stx-pat:expr x-pat:expr)
+     #'(? stx:is? (app stx:also-is-x->stx/x stx-pat x-pat))])
+  (id-transformer (λ (stx) #'stx/x->stx:also-is-x)))
+
 ;; Stx X -> [StxHas X]
-(define (stx-x->stx:has-x stx type)
-  (syntax-property stx prop:has type))
+(define (stx-x->stx:has-x stx x)
+  (syntax-property stx prop:has (it x)))
 
 ;; [StxHas X] -> (values Stx X)
 (define (stx:has-x->stx/x stx)
   (values (syntax-property stx prop:has #false)
-          (ca*r (syntax-property stx prop:has))))
+          (it-value (ca*r (syntax-property stx prop:has)))))
 
 ;; Stx X -> [StxHas X]
 ;; but also a match expander
