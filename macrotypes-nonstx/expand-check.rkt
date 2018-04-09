94e820d6da017d1e06254addef12d6ff1a584c17
AlexKnauth <alexander@knauth.org>
2018-04-09 18:38:24 -0400

    factor out a lot of `define-expand-check-function` into a syntax class

--- macrotypes-nonstx/expand-check.rkt
+++ macrotypes-nonstx/expand-check.rkt
@@ -25,56 +25,75 @@
       (split-at lst i))
     (append a (rest b))))
 
+(begin-for-syntax
+  (define-syntax-class (ec-function-decl name)
+    #:attributes [
+                  [def 1]            ; phase 0 definitions
+                  [function-expr 0]  ; phase 0 expression
+                  [ec-info-expr 0]   ; phase 1 expression
+                  ]
+
+    #:datum-literals [->]
+
+    [pattern [[in:id ... -> out:id ...]
+              #:in-stx in-stx:id
+              #:out-stx out-stx:id
+              (~optional (~seq #:context context)
+                         #:defaults ([context #''expression]))
+              #:stop-ids stop-ids:expr
+              #:bad-output bad-output:expr]
+     #:with name-in (format-id name "~a-in" name)
+     #:with name-out (format-id name "~a-out" name)
+     #:with expand/name-in (format-id name "expand/~a-in" name)
+     #:with name-in-prop (format-id #'here "~a-in-prop" name)
+     #:with name-out-prop (format-id #'here "~a-out-prop" name)
+     #:with [in-fld ...] (remove #'in-stx (attribute in) free-identifier=?)
+     #:with [out-fld ...] (remove #'out-stx (attribute out) free-identifier=?)
+     #:with N-in (length (attribute in))
+     #:with N-out (length (attribute out))
+     #:with in-stx-index (index-of (attribute in) #'in-stx free-identifier=?)
+     #:with out-stx-index (index-of (attribute out) #'out-stx free-identifier=?)
+     ;; -------------------------------------------------------------------
+     #:with [def ...]
+     #'[(struct name-in-prop [in-fld ...] #:prefab)
+        (struct name-out-prop [out-fld ...] #:prefab)
+        (define/match-expander name-in
+          (λ (in ...) (stx:has in-stx (name-in-prop in-fld ...)))
+          (make-expand-check-match-transformer
+           #'name-in-prop 'N-in 'in-stx-index #f))
+        (define/match-expander name-out
+          (λ (out ...) (stx:has (wrap-syntax/stop out-stx)
+                                (name-out-prop out-fld ...)))
+          (make-expand-check-match-transformer
+           #'name-out-prop 'N-out 'out-stx-index #'wrap-syntax/stop))
+        (define (expand/name-in in ...)
+          (expand/#%var (name-in in ...) context stop-ids))]
+     #:with function-expr
+     #'(λ (in ...)
+         (match (expand/name-in in ...)
+           [(name-out out ...)
+            (values out ...)]
+           [_
+            bad-output]))
+     #:with ec-info-expr
+     #'(make-expand-check-info
+        #'name-in
+        'N-in
+        #'name-out
+        'N-out
+        #'expand/name-in)]))
+
 (define-syntax-parser define-expand-check-function
-  #:datum-literals [->]
-  [(_ name:id
-      [in:id ... -> out:id ...]
-      #:in-stx in-stx:id
-      #:out-stx out-stx:id
-      (~optional (~seq #:context context) #:defaults ([context #''expression]))
-      #:stop-ids stop-ids:expr
-      #:bad-output bad-output:expr)
-   #:with name-in (format-id #'name "~a-in" #'name)
-   #:with name-out (format-id #'name "~a-out" #'name)
-   #:with expand/name-in (format-id #'name "expand/~a-in" #'name)
-   #:with name-in-prop (format-id #'here "~a-in-prop" #'name)
-   #:with name-out-prop (format-id #'here "~a-out-prop" #'name)
-   #:with [in-fld ...] (remove #'in-stx (attribute in) free-identifier=?)
-   #:with [out-fld ...] (remove #'out-stx (attribute out) free-identifier=?)
-   #:with N-in (length (attribute in))
-   #:with N-out (length (attribute out))
-   #:with in-stx-index (index-of (attribute in) #'in-stx free-identifier=?)
-   #:with out-stx-index (index-of (attribute out) #'out-stx free-identifier=?)
+  [(_ name:id . (~var decl (ec-function-decl #'name)))
    #'(begin
-       (struct name-in-prop [in-fld ...] #:prefab)
-       (struct name-out-prop [out-fld ...] #:prefab)
-       (define/match-expander name-in
-         (λ (in ...) (stx:has in-stx (name-in-prop in-fld ...)))
-         (make-expand-check-match-transformer
-          #'name-in-prop 'N-in 'in-stx-index #f))
-       (define/match-expander name-out
-         (λ (out ...) (stx:has (wrap-syntax/stop out-stx) (name-out-prop out-fld ...)))
-         (make-expand-check-match-transformer
-          #'name-out-prop 'N-out 'out-stx-index #'wrap-syntax/stop))
-       (define (expand/name-in in ...)
-         (expand/#%var (name-in in ...) context stop-ids))
+       decl.def
+       ...
        (define/syntax-info name
-         (λ (in ...)
-           (match (expand/name-in in ...)
-             [(name-out out ...)
-              (values out ...)]
-             [_
-              bad-output]))
+         decl.function-expr
          (λ (internal-name)
            (expand-check-info*
             internal-name
-            (make-expand-check-info
-             #'name-in
-             'N-in
-             #'name-out
-             'N-out
-             #'expand/name-in))))
-       )])
+            decl.ec-info-expr))))])
 
 
 
