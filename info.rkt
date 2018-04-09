d493661833aa074144c6bdd00b2996118170ecec
AlexKnauth <alexander@knauth.org>
2018-04-08 23:02:22 -0400

    add info.rkt

---
+++ info.rkt
@@ -0,0 +1,23 @@
+#lang info
+
+;; -----------------------------------------------
+
+;; Package Info
+
+(define collection "macrotypes-nonstx")
+
+(define deps
+  '("base"
+    "agile"
+    "rackunit-lib"
+    "syntax-classes-lib"
+    ))
+
+;; -----------------------------------------------
+
+;; Collection Info
+
+;; (currently none)
+
+;; -----------------------------------------------
+
